23f11e77dcd37869f14ef2a16323de6da1e95deb
Edu-Hiroshi <97803912+Edu-Hiroshi@users.noreply.github.com>
2022-03-20 00:00:55 -0300

    Update contador_decrescente.vhd

--- Projeto_final/contador_decrescente.vhd
+++ Projeto_final/contador_decrescente.vhd
@@ -16,42 +16,33 @@ entity contador_decrescente is
         clock   : in  std_logic;
         reset   : in  std_logic;
         conta   : in  std_logic;
-		acertou : in  std_logic;
 		limite  : in  integer;
 		timeout : out std_logic 
    );
 end contador_decrescente;
 
 architecture comportamental of contador_decrescente is
-    signal tempo: integer range 0 to limite - 1; -- Declaração do sinal interno de contagem
+    signal tempo: integer range 0 to limite - 1; -- sinal de contagem
 begin
-  
     process (clock, reset, conta, tempo)
-    
 	begin
         if reset = '1' then    
 		    tempo <= 0;   
         
 		elsif rising_edge(clock) then
-
 			if conta = '1' then 
                 if tempo = limite - 1 then 
 				    tempo <= 0; 
-                
 				else           
 				    tempo <= tempo + 1; 
                 end if;
-            
 			else               
 			    tempo <= tempo;
             end if;
-			
         end if;
-    
 	end process;
 
     -- saida timeout
     timeout <= '1' when tempo = limite - 1 else
                '0';
-		   
 end comportamental;
