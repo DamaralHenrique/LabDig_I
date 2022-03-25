4c2422e91d6d415e71be9fdcb2692333e6b64bd5
Edu-Hiroshi <97803912+Edu-Hiroshi@users.noreply.github.com>
2022-03-25 10:32:18 -0300

    Update registrador_173.vhd

--- Projeto_final/registrador_173.vhd
+++ Projeto_final/registrador_173.vhd
@@ -1,13 +1,14 @@
 -----------------------------------------------------------------
 -- Arquivo   : registrador_173.vhd
--- Projeto   : Experiencia 3 - Projeto de uma Unidade de Controle
+-- Projeto   : Projeto Tapa no Tatu
 -----------------------------------------------------------------
--- Descricao : registrador de 4 bits
+-- Descricao : registrador de 4 bits (adapatado para 6)
 --             similar ao CI 74173
 -----------------------------------------------------------------
 -- Revisoes  :
 --     Data        Versao  Autor             Descricao
 --     18/01/2022  1.0     Edson Midorikawa  versao inicial
+--     25/03/2022  2.0     Eduardo Hiroshi   versao adaptada para 6 bits
 -----------------------------------------------------------------
 
 library ieee;
@@ -19,8 +20,8 @@ entity registrador_173 is
         clear : in  std_logic;
         en1   : in  std_logic;
         en2   : in  std_logic;
-        D     : in  std_logic_vector (3 downto 0);
-        Q     : out std_logic_vector (3 downto 0)
+        D     : in  std_logic_vector (5 downto 0);
+        Q     : out std_logic_vector (5 downto 0)
    );
 end entity registrador_173;
 
@@ -30,7 +31,7 @@ begin
     process (clock, clear)
     begin
         if clear='1' then -- Clear assíncrono ativo alto
-            Q <= "0000";  -- Zera saída
+            Q <= "000000";  -- Zera saída
         elsif clock'event and clock='1' then -- Na borda de subida
             if en1='0' and en2='0'then  -- Se ambos os enables estiverem em 0 (Ativos baixo)
                 Q <= D; -- Saída recebe valor da entrada D
